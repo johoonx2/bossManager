09fbaa815796bd09af4952c2d7492cce459642a2
hoonjo <hoonjo@linecorp.com>
2018-11-11 14:53:00 +0900

    [조훈] Tutorial 1. Hello world - 기본 연결 테스트 Ref. https://www.youtube.com/watch?v=fRg61Pr6Hjs&list=PLPS49xU25mxM6n2ZoXDXJRtdXYSr7NT2h

---
+++ src/main/java/com/bot/discord/controller/Main.java
@@ -0,0 +1,20 @@
+package com.bot.discord.controller;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] Args) throws LoginException, RateLimitedException, InterruptedException{
+        JDABuilder builder = new JDABuilder((AccountType.BOT));
+        builder.setToken(Args[0]);
+        builder.setAutoReconnect(true);
+        builder.setStatus(OnlineStatus.ONLINE);
+        JDA jda =  builder.buildBlocking();
+
+    }
+}
