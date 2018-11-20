9d860811356b91f129ab77d9e0665cbecaff4d65
hoonjo <hoonjo@linecorp.com>
2018-11-20 20:50:07 +0900

    [조훈] Log 추가

--- src/main/java/com/bot/discord/core/permsCore.java
+++ src/main/java/com/bot/discord/core/permsCore.java
@@ -10,10 +10,13 @@ public class permsCore {
 
     public static boolean check(MessageReceivedEvent event) {
 
+        System.out.println("[INFO] check func Called.");
         for( Role r : event.getGuild().getMember(event.getAuthor()).getRoles()){
             if(Arrays.stream(STATIC.PERMS).parallel().anyMatch(r.getName() :: contains)){
+                System.out.println("[INFO] Permission Allowed. (check)");
                 return false;
             }else{
+                System.out.println("[INFO] Permission Not Allowed. (check)");
                 event.getTextChannel().sendMessage(":warning: sorry, " + event.getAuthor().getAsMention() + ", you dont't have the permissions to use this command!").queue();
             }
         }
